2de66888945a26af612451a91356f01d6c493e46
Ritam Sarmah <ritamsarmah@Ritams-MacBook-Pro.local>
2016-07-11 11:30:54 -0700

    First commit

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+}
